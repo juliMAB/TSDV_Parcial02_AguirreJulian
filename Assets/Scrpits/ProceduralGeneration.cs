15c22c4000b865c28df68f50ecfdc44c5d6ac9ac
juliMAB <julian6aguirre@hotmail.com>
2021-06-25 17:53:43 -0300

    Now we can generate landzones

--- Assets/Scrpits/ProceduralGeneration.cs
+++ Assets/Scrpits/ProceduralGeneration.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.U2D;
 
@@ -8,7 +9,7 @@ public class ProceduralGeneration : MonoBehaviour
     [SerializeField] int minHeight, maxHeight;
     [Range(6,100)]
     [SerializeField] int cantNodosExtras;
-    [SerializeField] int widht;
+    [SerializeField] int landZones;
     [SerializeField] SpriteShapeController sSC;
     [SerializeField] GameObject bounds;
 
@@ -21,11 +22,25 @@ public class ProceduralGeneration : MonoBehaviour
         {
             sSC.spline.InsertPointAt(i, Vector3.up* Random.Range(minHeight,maxHeight)+posinitial +Vector3.right*i* bounds.transform.localScale.x/(cantNodosExtras-1));
         }
+        for (int i = 0; i < landZones; i++)
+        {
+            int index = Random.Range(3, cantNodosExtras - 2);
+            sSC.spline.SetPosition (index, new Vector3 ( sSC.spline.GetPosition(index).x, sSC.spline.GetPosition(index-1).y,0));
+        }
         sSC.spline.InsertPointAt(cantNodosExtras, posinitial +Vector3.right * bounds.transform.localScale.x);
         sSC.spline.InsertPointAt(cantNodosExtras+1, posinitial);
     }
     void Start()
     {
+        //if (sSC==null)
+        //{
+        //    GameObject go = new GameObject();
+        //    go.name = "GROUND";
+        //    go.AddComponent<SpriteShapeController>();
+        //    go.GetComponent<SpriteShapeController>().spriteShape.fillTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/ART/Sprites/Nature2.jpg", typeof(Texture2D));
+        
+        //}
+        
         terrenogernera();
     }
     private void Update()
@@ -34,7 +49,6 @@ public class ProceduralGeneration : MonoBehaviour
             terrenogernera();
         }
     }
-
 
 
     GameObject spawnObj(GameObject obj,int width,int height)//What ever we spawn will be a child of our procedural generation gameObj
