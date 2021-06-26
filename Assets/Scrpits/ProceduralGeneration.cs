4696fcdefdc0b510278379a08ebf96aa7156ed19
juliMAB <julian6aguirre@hotmail.com>
2021-06-26 16:51:51 -0300

    CreanUp ProceduralGeneration

--- Assets/Scrpits/ProceduralGeneration.cs
+++ Assets/Scrpits/ProceduralGeneration.cs
@@ -7,17 +7,17 @@ using UnityEngine.U2D;
 
 public class ProceduralGeneration : MonoBehaviour
 {
+    [Tooltip("the next point takes a random height between these two values, adding the current height ")]
     [SerializeField] int minHeight, maxHeight;
-    
+    [Tooltip("height of the first point")]
     [SerializeField] int FirstHeight;
     [Range(6, 100)]
+    [Tooltip("number of points on the map")]
     [SerializeField] int cantNodosExtras;
-
     [Range(1, 6)]
     [SerializeField] int landZones;
 
-    [Tooltip("Components")]
-    [SerializeField] SpriteShapeController sSC;
+    [SerializeField] SpriteShapeController spriteShapeController;
     [SerializeField] GameObject bounds;
 
     [SerializeField] GameObject prefabText;
@@ -38,43 +38,43 @@ public class ProceduralGeneration : MonoBehaviour
 
     void terrenogernera()
     {
-        maxHight = bounds.transform.localScale.y / 1.5f;
+        maxHight = bounds.transform.localScale.y / 2;
         float hightPoint=0;
         zonesList.Clear();
         Vector3 posinitial = Vector3.left * bounds.transform.localScale.x/2 + Vector3.down * bounds.transform.localScale.y/2;
         Vector3 lastposition;
-        sSC.spline.Clear();
-        sSC.spline.InsertPointAt(0, Vector3.up * FirstHeight + posinitial);
-        lastposition = sSC.spline.GetPosition(0);
+        spriteShapeController.spline.Clear();
+        spriteShapeController.spline.InsertPointAt(0, Vector3.up * FirstHeight + posinitial);
+        lastposition = spriteShapeController.spline.GetPosition(0);
         zonesList.Add(zones.bounds);
         //generar terreno basico con elevaciones y bajadas.
         for (int i = 1; i < cantNodosExtras; i++)
         {
-            sSC.spline.InsertPointAt(i, Vector3.up * (Random.Range(minHeight, maxHeight) * 0.1f) + Vector3.up * lastposition.y + Vector3.right * posinitial.x + Vector3.right * i * bounds.transform.localScale.x / (cantNodosExtras - 1));
-            lastposition = sSC.spline.GetPosition(i);
-            if (hightPoint<= sSC.spline.GetPosition(i).y)
+            spriteShapeController.spline.InsertPointAt(i, Vector3.up * (Random.Range(minHeight, maxHeight) * 0.1f) + Vector3.up * lastposition.y + Vector3.right * posinitial.x + Vector3.right * i * bounds.transform.localScale.x / (cantNodosExtras - 1));
+            lastposition = spriteShapeController.spline.GetPosition(i);
+            if (hightPoint<= spriteShapeController.spline.GetPosition(i).y)
             {
-                hightPoint = sSC.spline.GetPosition(i).y;
+                hightPoint = spriteShapeController.spline.GetPosition(i).y;
             }
             //si esta por debajo del piso lo sube.
             if (posinitial.y+1 >= lastposition.y)
             {
-                sSC.spline.SetPosition(i, new Vector3(sSC.spline.GetPosition(i).x, posinitial.y+2, 0f));
-                lastposition = sSC.spline.GetPosition(i);
+                spriteShapeController.spline.SetPosition(i, new Vector3(spriteShapeController.spline.GetPosition(i).x, posinitial.y+2, 0f));
+                lastposition = spriteShapeController.spline.GetPosition(i);
             }
             //si esta por ensima del maximo, lo baja.
             else if (maxHight <= lastposition.y)
             {
-                sSC.spline.SetPosition(i, new Vector3(sSC.spline.GetPosition(i).x, maxHight - 2, 0f));
-                lastposition = sSC.spline.GetPosition(i);
+                spriteShapeController.spline.SetPosition(i, new Vector3(spriteShapeController.spline.GetPosition(i).x, maxHight - 2, 0f));
+                lastposition = spriteShapeController.spline.GetPosition(i);
             }
             zonesList.Add(zones.normal);
         }
         
 
         //generar zonas de aterrizaje que sean rectas.
-        sSC.spline.InsertPointAt(cantNodosExtras, posinitial + Vector3.right * bounds.transform.localScale.x);
-        sSC.spline.InsertPointAt(cantNodosExtras + 1, posinitial);
+        spriteShapeController.spline.InsertPointAt(cantNodosExtras, posinitial + Vector3.right * bounds.transform.localScale.x);
+        spriteShapeController.spline.InsertPointAt(cantNodosExtras + 1, posinitial);
         zonesList.Add(zones.bounds);
         foreach (Transform child in textConteiner.transform)
         {
@@ -89,8 +89,8 @@ public class ProceduralGeneration : MonoBehaviour
             }
             else
             {
-                sSC.spline.SetPosition(index, new Vector3(sSC.spline.GetPosition(index).x, sSC.spline.GetPosition(index - 1).y, 0));
-                GameObject go = Instantiate(prefabText, sSC.spline.GetPosition(index), Quaternion.identity, textConteiner.transform);
+                spriteShapeController.spline.SetPosition(index, new Vector3(spriteShapeController.spline.GetPosition(index).x, spriteShapeController.spline.GetPosition(index - 1).y, 0));
+                GameObject go = Instantiate(prefabText, spriteShapeController.spline.GetPosition(index), Quaternion.identity, textConteiner.transform);
                 go.name = "LandZone" + i;
                 zonesList[index--] = zones.save; index++;
                 zonesList[index] = zones.save;
@@ -100,15 +100,6 @@ public class ProceduralGeneration : MonoBehaviour
     }
     void Start()
     {
-        //if (sSC==null)
-        //{
-        //    GameObject go = new GameObject();
-        //    go.name = "GROUND";
-        //    go.AddComponent<SpriteShapeController>();
-        //    go.GetComponent<SpriteShapeController>().spriteShape.fillTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/ART/Sprites/Nature2.jpg", typeof(Texture2D));
-        
-        //}
-        
         terrenogernera();
     }
     private void Update()
