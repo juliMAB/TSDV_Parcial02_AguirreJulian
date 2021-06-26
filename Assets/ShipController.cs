f523a2638e4b8f6e2d8d14dcaf058be97c81137c
juliMAB <julian6aguirre@hotmail.com>
2021-06-26 16:51:29 -0300

    Add shipControler and ShipManager

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    [SerializeField] float MotorForce;
+    [SerializeField] float rotationIntervalsOnDgres;
     private Rigidbody2D rb2d;
+    public Rigidbody2D rb { get { return rb2d; } }
+    private ParticleSystem particleS;
+
+
+    public Action OnFuelUse;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        particleS = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -16,15 +25,25 @@ public class ShipController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward, -45, Space.Self);
+            transform.Rotate(Vector3.forward, rotationIntervalsOnDgres, Space.Self);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward, 45, Space.Self);
+            transform.Rotate(Vector3.forward, -rotationIntervalsOnDgres, Space.Self);
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb2d.AddForce(transform.up);
+            rb2d.AddForce(transform.up* MotorForce * 0.0001f);
+            particleS.Play();
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.AddForce(transform.up * MotorForce*0.0001f);
+            particleS.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            particleS.Stop();
         }
     }
 }
---
+++ Assets/ShipManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManager : ShipController
+{
+    public Action OnDestroy;
+    [SerializeField] float maxVelocityToLand;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEnter " + collision.collider.name);
+        if (Mathf.Abs (rb.velocity.x)+ Mathf.Abs(rb.velocity.y)>maxVelocityToLand)
+        {
+            print("Explote!");
+            OnDestroy?.Invoke();
+        }
+        
+    }
+}
