4dbcb08993a683bcc816ad47b5d902d043c0d309
juliMAB <julian6aguirre@hotmail.com>
2021-06-26 03:04:39 -0300

    Add shipControler

---
+++ Assets/ShipController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, -45, Space.Self);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward, 45, Space.Self);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(transform.up);
+        }
+    }
+}
