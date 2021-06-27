967aba92650b2f1e7d803526d0897c6b2efa822b
juliMAB <julian6aguirre@hotmail.com>
2021-06-26 21:01:30 -0300

    Add script for Paralax

---
+++ Assets/Paralax.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    [SerializeField] GameObject layer1;
+    [SerializeField] GameObject layer2;
+    [SerializeField] GameObject layer3;
+    [SerializeField] GameObject layer4;
+    [SerializeField] GameObject layer5;
+    [SerializeField] GameObject ship;
+    [SerializeField] Vector3 offset2;
+    [SerializeField] Vector3 offset3;
+    [SerializeField] Vector3 offset4;
+    [SerializeField] Vector3 offset5;
+    private void Start()
+    {
+        offset2 = layer2.transform.position;
+        offset3 = layer3.transform.position;
+        offset4 = layer4.transform.position;
+        offset5 = layer5.transform.position;
+
+    }
+
+    private void Update()
+    {
+        layer1.transform.position = ship.transform.position * -1/2;
+        layer2.transform.position = ship.transform.position * -1/3 + offset2;
+        layer3.transform.position = ship.transform.position * -1/6 + offset3;
+        layer4.transform.position = ship.transform.position * -1/10 + offset4;
+        layer5.transform.position = ship.transform.position * -1/16 + offset5;
+    }
+}
