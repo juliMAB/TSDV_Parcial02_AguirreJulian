6af6884fafdfc827211857400365eed054980d48
juliMAB <julian6aguirre@hotmail.com>
2021-06-27 14:26:23 -0300

    Add canvasManager

---
+++ Assets/canvasManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class canvasManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI score          ;
+    [SerializeField] TextMeshProUGUI timer          ;
+    [SerializeField] TextMeshProUGUI horizontalSpeed;
+    [SerializeField] TextMeshProUGUI verticalSpeed  ;
+    [SerializeField] TextMeshProUGUI altitude       ;
+    [SerializeField] LevelManager levelManager      ;
+    [Header("ref2")]
+    private ShipData shipData;
+    [SerializeField] private int modificador=10;
+    
+
+
+    void Start()
+    {
+        shipData = levelManager.shipController.GetData();
+    }
+    void Update()
+    {
+        timer.text = levelManager.timeInGame.ToString();
+
+        horizontalSpeed.text = "HorizontalSpeed: " + Mathf.RoundToInt(shipData.rb2d.velocity.x* modificador);
+
+        verticalSpeed.text = "VerticalSpeed: " + Mathf.RoundToInt(shipData.rb2d.velocity.y * modificador);
+
+        altitude.text = "Altitude: " + Mathf.RoundToInt(shipData.altitude * modificador);
+    }
+}
