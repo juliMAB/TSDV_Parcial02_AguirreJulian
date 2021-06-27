6bbaf8ee71a0fda223c174ce10cd890126702d9a
juliMAB <julian6aguirre@hotmail.com>
2021-06-27 04:23:20 -0300

    Add camera follow player on zoom

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] ShipManager ship;
+    Vector3 initialpos;
+    private Camera maincamera;
+
+    private void Start()
+    {
+        maincamera = Camera.main;
+        initialpos = transform.position;
+        ship.OnZoomIn += ZoomIn;
+        ship.OnZoomOut += ZoomOut;
+    }
+
+    void ZoomIn()
+    {
+        maincamera.orthographicSize /= 2;
+        transform.position = ship.transform.position + Vector3.forward * initialpos.z ;
+    }
+    void ZoomOut()
+    {
+        maincamera.orthographicSize *= 2;
+        transform.position = initialpos;
+    }
+}
