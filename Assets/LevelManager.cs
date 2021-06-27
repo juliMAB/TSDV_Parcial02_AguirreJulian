2896804eb888193dc2ef0e7677d52de0a4cd75e1
juliMAB <julian6aguirre@hotmail.com>
2021-06-27 14:25:42 -0300

    Add LevelManager to save score and time

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonoBehaviourSingletonScript;
+
+public class LevelManager : MonoBehaviourSingleton<LevelManager>
+{
+    [SerializeField] int score;
+    [SerializeField] public int timeInGame;
+
+    public GameObject player;
+    public ShipController shipController;
+    public ShipManager shipManager;
+
+    void Start()
+    {
+        shipController = player.GetComponent<ShipController>();
+        shipManager = player.GetComponent<ShipManager>();
+    }
+
+    public void AddScore(int _score)
+    {
+        score += _score;
+    }
+
+    void LoseMatch()
+    {
+        //mostrar la derrota.
+        //sacarte y llevarte al scoreboard.
+    }
+
+    void WinMatch()
+    {
+        //mostrar la victoria.
+        //sumar los puntos.
+        //cargar siguiente nivel.
+    }
+}
