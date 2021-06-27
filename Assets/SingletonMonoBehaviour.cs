26c9ef74a71f5fd1303cec6f45e2dc9482fce0b6
juliMAB <julian6aguirre@hotmail.com>
2021-06-27 14:25:06 -0300

    Add a singleton.cs

---
+++ Assets/SingletonMonoBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MonoBehaviourSingletonScript
+{
+    public class MonoBehaviourSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+        public static T Get()
+        {
+            return instance;
+        }
+
+        public virtual void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
