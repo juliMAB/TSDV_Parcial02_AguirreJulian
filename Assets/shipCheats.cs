4123ac41ddb9a102ecb93d4254468bf7a9ff0d82
juliMAB <julian6aguirre@hotmail.com>
2021-06-27 04:22:10 -0300

    Create shipCheats.cs

---
+++ Assets/shipCheats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipCheats : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.position += (Vector3)Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            transform.position += (Vector3)Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            transform.position += (Vector3)Vector2.down;
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            transform.position += (Vector3)Vector2.right;
+        }
+
+    }
+}
