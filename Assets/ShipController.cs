d5341a49bf4366d6278aff2b8e83cf1ee4fa8170
juliMAB <julian6aguirre@hotmail.com>
2021-06-27 04:22:37 -0300

    Updaye zoom

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -6,22 +6,25 @@ using UnityEngine;
 public class ShipController : MonoBehaviour
 {
     [SerializeField] float MotorForce;
+    [SerializeField] float fuel;
     [SerializeField] float rotationIntervalsOnDgres;
+    [SerializeField] float zoomDistance;
+    [SerializeField] LayerMask floorLayer;
+
+
+    float distancetoFloor;
     private Rigidbody2D rb2d;
     public Rigidbody2D rb { get { return rb2d; } }
     private ParticleSystem particleS;
-
-
     public Action OnFuelUse;
-    // Start is called before the first frame update
+    
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         particleS = GetComponent<ParticleSystem>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -35,15 +38,28 @@ public class ShipController : MonoBehaviour
         {
             rb2d.AddForce(transform.up* MotorForce * 0.0001f);
             particleS.Play();
+            UseFuel();
         }
         else if (Input.GetKey(KeyCode.Space))
         {
             rb2d.AddForce(transform.up * MotorForce*0.0001f);
             particleS.Play();
+            UseFuel();
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
             particleS.Stop();
         }
+        //actualizar la distacian al piso.
+        distancetoFloor = Vector3.Distance(transform.position,(Physics2D.Raycast(transform.position+transform.localScale/2, Vector2.down)).point);
+    }
+    void UseFuel()
+    {
+        fuel -= 0.1f;
+        OnFuelUse?.Invoke();
+    }
+    public bool OnZoomDistance()
+    {
+        return Physics2D.OverlapCircle(transform.position, zoomDistance, floorLayer) !=null;
     }
 }
