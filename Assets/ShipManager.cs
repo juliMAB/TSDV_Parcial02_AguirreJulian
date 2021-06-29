a801bd5f04b00d8969942e937bf5cd4ff0063e17
juliMAB <julian6aguirre@hotmail.com>
2021-06-28 22:43:16 -0300

    Add Onland Action to know when ship arrives

--- Assets/ShipManager.cs
+++ Assets/ShipManager.cs
@@ -11,11 +11,18 @@ public class ShipManager : MonoBehaviour
     public Action OnFastMove;
     public Action OnZoomIn;
     public Action OnZoomOut;
+    public Action OnLanding;
     //------------------------
     [SerializeField] GameObject destroyShip;
     bool zoomIn=false;
     ShipController ShipController;
     Vector2 lastVelocity;
+    float ofset;
+    RaycastHit2D hitImpact;
+    public ShipData GetData()
+    {
+        return data;
+    }
 
     private void Start()
     {
@@ -23,8 +30,8 @@ public class ShipManager : MonoBehaviour
         data.particleS = GetComponent<ParticleSystem>();
         ShipController = GetComponent<ShipController>();
         ShipController.setData = data;
-        OnDestroy += Destroing; 
-
+        OnDestroy += Destroing;
+        ofset = transform.GetComponent<BoxCollider2D>().size.y/2;
     }
     void Destroing()
     {
@@ -33,13 +40,24 @@ public class ShipManager : MonoBehaviour
         //Instantiate(destroyShip,transform.position,Quaternion.identity, (new GameObject("Basura")).transform);
         //Destroy(gameObject, 1);
     }
+    public void hide()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<ParticleSystem>().Pause();
+    }
+    public void show()
+    {
+
+    }
     private void FixedUpdate()
     {
         lastVelocity = data.rb2d.velocity;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Mathf.RoundToInt((Mathf.Abs (lastVelocity.x)+ Mathf.Abs(lastVelocity.y)))*10>data.maxVelocityToLanding)
+
+        if (Mathf.RoundToInt((Mathf.Abs(lastVelocity.x) + Mathf.Abs(lastVelocity.y))) * 10 > data.maxVelocityToLanding /*|| Vector2.Distance(hitImpact.normal, transform.position) > 10*/)
         {
             print("velocidad actual: " + Mathf.RoundToInt( (Mathf.Abs(lastVelocity.x * 10) + Mathf.Abs(lastVelocity.y * 10))));
             print("Explote!");
@@ -47,7 +65,7 @@ public class ShipManager : MonoBehaviour
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             OnDestroy?.Invoke();
         }
-        print("Choque!");
+        //print("Choque!");
     }
     bool OnZoomDistance()
     {
@@ -68,6 +86,34 @@ public class ShipManager : MonoBehaviour
                 print("Entrando al zoom");
                 zoomIn = true;
                 OnZoomIn?.Invoke();
+            }
+        }
+
+        //actualizar la distacian al piso.
+        hitImpact = (Physics2D.Raycast(transform.position ,Vector2.down,20,data.layerfloor));
+        data.altitude = Vector3.Distance(transform.position+ Vector3.down*ofset, hitImpact.point);
+        circulo(hitImpact.point);
+        Debug.DrawRay(transform.position +Vector3.down* ofset, Vector3.down);
+    }
+    void circulo(Vector3 a)
+    {
+        Debug.DrawLine(a, a+Vector3.left);
+        Debug.DrawLine(a, a+Vector3.right);
+        Debug.DrawLine(a, a+Vector3.up);
+        Debug.DrawLine(a, a+Vector3.down);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject.tag == "LandingZone")
+        {
+            if (data.rb2d.velocity.y == 0)
+            {
+                if (transform.up == Vector3.up)
+                {
+                    OnLanding?.Invoke();
+                    print("aterize!");
+                    Destroy(collision.gameObject);
+                }
             }
         }
     }
