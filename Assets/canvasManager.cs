3e4745fd136d56b79a4fe361c0cb6c4795384030
juliMAB <julian6aguirre@hotmail.com>
2021-06-28 22:40:49 -0300

    Update canvas to show time

--- Assets/canvasManager.cs
+++ Assets/canvasManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class canvasManager : MonoBehaviour
 {
@@ -11,24 +12,34 @@ public class canvasManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI verticalSpeed  ;
     [SerializeField] TextMeshProUGUI altitude       ;
     [SerializeField] LevelManager levelManager      ;
+    [SerializeField] Slider sliderFuel              ;
     [Header("ref2")]
     private ShipData shipData;
     [SerializeField] private int modificador=10;
-    
+    private int minutos;
+    private int segundos;
+
 
 
     void Start()
     {
-        shipData = levelManager.shipController.GetData();
+        shipData = levelManager.shipManager.GetData();
+        sliderFuel.maxValue = shipData.initialfuel;
+
     }
     void Update()
     {
-        timer.text = levelManager.timeInGame.ToString();
+        segundos = Mathf.RoundToInt(levelManager.timeGame % 60);
+        minutos  = Mathf.RoundToInt(levelManager.timeGame / 60);
+
+        timer.text = minutos +":"+segundos;
 
         horizontalSpeed.text = "HorizontalSpeed: " + Mathf.RoundToInt(shipData.rb2d.velocity.x* modificador);
 
         verticalSpeed.text = "VerticalSpeed: " + Mathf.RoundToInt(shipData.rb2d.velocity.y * modificador);
 
-        altitude.text = "Altitude: " + Mathf.RoundToInt(shipData.altitude * modificador);
+        altitude.text = "Altitude: " + Mathf.RoundToInt(shipData.altitude * 10);
+
+        sliderFuel.value = shipData.fuel;
     }
 }
